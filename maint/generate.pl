d3171aef03ceeaa4e0830d69195391f7c7f5c806
Shoichi Kaji <skaji@cpan.org>
2020-07-13 22:28:21 +0900

    update

--- maint/generate.pl
+++ maint/generate.pl
@@ -11,9 +11,7 @@ sub warn_and_exit {
 }
 
 my $releases = eval { CPAN::Perl::Releases::MetaCPAN->new->get };
-if (my $err = $@) {
-    warn_and_exit $@;
-}
+warn_and_exit $@ if $@;
 
 if (my @empty = grep { !$_->{download_url} } $releases->@*) {
     warn_and_exit "Empty download_url for ", (join ", ", map { $_->{name} } @empty);
@@ -21,11 +19,11 @@ if (my @empty = grep { !$_->{download_url} } $releases->@*) {
 
 my @release;
 for my $r ($releases->@*) {
-    my ($version, $major, $minor, $RC) = $r->{name} =~ /^perl-(5\.(\d+)\.(\d+)(?:-(?:RC|TRIAL)(\d+))?)$/;
+    my ($version, $major, $minor, $patch, $RC) = $r->{name} =~ /^perl-((\d)\.(\d+)\.(\d+)(?:-(?:RC|TRIAL)(\d+))?)$/;
     next if !$major;
-    my $key = sprintf "5.%03d.%03d.%03d", $major, $minor, (defined $RC ? $RC : 999);
+    my $key = sprintf "%d.%03d.%03d.%03d", $major, $minor, $patch, (defined $RC ? $RC : 999);
     next if $key lt "5.008.001.000";
-    my $status = $major % 2 == 1 ? "unstable" : defined $RC ? "testing" : "stable";
+    my $status = $minor % 2 == 1 ? "unstable" : defined $RC ? "testing" : "stable";
     my $download_url = $r->{download_url};
     my $gz_url = $download_url =~ s/\.(bz2|xz)$/.gz/r;
     my $xz_url = $key gt "5.021.005.999" && $version ne "5.23.6" ? $gz_url =~ s/\.gz$/.xz/r : "NA";
